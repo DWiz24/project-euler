7f5b9bf92f0caf3754c297dd26da4cc944cfd288
joeshmoe999 <bobthequark@gmail.com>
2015-06-09 12:07:17 -0400

    Update #7.java

--- #7.java
+++ #7.java
@@ -1,1 +1,13 @@
-
+public class test {
+  public static void main(String[] a) {
+    int primes=0;
+    for (int i=0; primes<=10001; i++) {
+      boolean isPrime=true;
+      for (int j=2; j<=i/2 && isPrime; j++) {
+        if (i%j==0) isPrime=false;
+      }
+      if (isPrime) primes++;
+      if (primes==10001) System.out.println(i);
+    }
+  }
+}
