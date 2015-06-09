2c3a881194f706925cb9c4226562cc845c8997c9
joeshmoe999 <bobthequark@gmail.com>
2015-06-09 09:45:24 -0400

    Update #2.java

--- #2.java
+++ #2.java
@@ -1,1 +1,14 @@
-
+public class test {
+  public static void main(String[] a) {
+    int evens=0;
+    int prev=1;
+    int past2=0;
+    while (prev+past2<=4000000) {
+      if ((prev+past2)%2==0) evens += prev+past2;
+      int t=past2+prev;
+      past2=prev;
+      prev=t;
+    }
+    System.out.println(evens);
+  }
+}
